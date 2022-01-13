0bf9b058e7759eacb25267e4e315276dc813097d
Andrew Zhang <andrew_zzh@hotmail.com>
2022-01-12 21:48:15 -0500

    2022-01-12-03

--- c-cpp/junior/2013/question1.c
+++ c-cpp/junior/2013/question1.c
@@ -5,4 +5,15 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]){
+    int youngest = 0;
+    int middle = 0;
+
+    scanf("%d", &youngest);
+    scanf("%d", &middle);
+
+    int diff = middle - youngest;
+
+    int oldest = middle + diff;
+
+    printf("%d\n", oldest);
 }
