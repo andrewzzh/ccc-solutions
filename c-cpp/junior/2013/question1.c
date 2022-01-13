c64e98e38537a5aef009170879b0ee2166d4c0b6
Andrew Zhang <andrew_zzh@hotmail.com>
2022-01-12 19:48:38 -0500

    2022-01-12-02

---
+++ c-cpp/junior/2013/question1.c
@@ -0,0 +1,8 @@
+/*************************************
+* Problem J1: Next in line
+* assumption: inputs are integers
+**************************************/
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+}
